22c2b5a96d4e26b2228c4b97b525ee65c8533a4e
Nancy Meike <nancy.meike@deutschebahn.com>
2023-06-02 18:42:17 +0200

    updated fixed script line

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { bootstrapExtra } from "@workadventure/scripting-api-extra"
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+
 bootstrapExtra();
 
 console.log('Script started successfully');
